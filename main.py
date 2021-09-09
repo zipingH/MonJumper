72e49c1825065c25ee1c20d2d7d9dbcdd403ae57
Ziping Huang <tomhuang1997@gmail.com>
2021-09-08 21:32:34 -0700

    Imported pygame and created a resizable screen.

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 # MonJumper is a game that will be similar to geometry dash.
 
+#import modules
+import pygame
+
+
+
+
+def initialize():
+    # Intialize the pygame
+    pygame.init()
+
+    #create a resizeable screen  (withd, height)
+    screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
+
+    # Game loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
 def main():
-    print("Hello World")
+    initialize()
+
 
 if __name__=='__main__':
     main()
